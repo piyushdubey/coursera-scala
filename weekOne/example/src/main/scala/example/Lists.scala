1a06f4315ff9da265f7894bc9e9a04ea68df9909
Piyush Dubey <piyush.dubey@servicenow.com>
2017-07-16 23:53:59 -0700

    Week 1 - Practice assignment solution

--- weekOne/example/src/main/scala/example/Lists.scala
+++ weekOne/example/src/main/scala/example/Lists.scala
@@ -23,7 +23,16 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+
+      def inner(ls: List[Int], total: Int): Int = {
+        ls match {
+          case x :: tail => inner(tail, total+x)
+          case Nil => total
+        }
+      }
+      inner(xs, 0)
+    }
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +47,8 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if(xs.isEmpty) throw new NoSuchElementException
+      xs.reduceLeft((x,y) => if(x>y) x else y)
+    }
   }
